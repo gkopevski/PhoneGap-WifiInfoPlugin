bfedfd622c4d4d57897160ce90de38e7fbfee23e
Goran Kopevski <gkopevski@gmail.com>
2014-09-26 14:23:03 +0200

    Update cordovaWifiInfo.js

--- www/cordovaWifiInfo.js
+++ www/cordovaWifiInfo.js
@@ -1,18 +1,31 @@
-window.wifi = function(callback) {
-    cordova.exec(
-            function(data){callback(data);},
-            function(err){callback(err);},
-            "WifiInfo",
-            'getInfo',
-            []
-    );
-};
-window.getSavedWifiNetworks = function(callback) {
-    cordova.exec(
-            function(data){callback(data);},
-            function(err){callback(err);},
-            "WifiInfo",
-            'getSavedWifiNetworks',
-            []
-    );
-};
+var exec = require("cordova/exec");
+var wifi = {
+		wifi: function(callback) {
+            cordova.exec(
+                    function(data) {
+                        callback(data);
+                    },
+                    function(err) {
+                        callback(err);
+                    },
+                    "WifiInfo",
+                    'getInfo',
+                    []
+                    );
+        },
+        getSavedWifiNetworks: function(callback) {
+			cordova.exec(
+                    function(data) {
+                        callback(data);
+                    },
+                    function(err) {
+                        callback(err);
+                    },
+                    "WifiInfo",
+                    'getSavedWifiNetworks',
+                    []
+                    );
+        }
+}
+
+module.exports = wifi;
